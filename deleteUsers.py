61d5a8a1ee74cdd41964c7e7d47a4c6a55da3055
Gabriel Torres <Gabrieltorres2014@gmail.com>
2023-11-22 18:18:55 -0400

    Recursive function - clicking all buttons

--- deleteUsers.py
+++ deleteUsers.py
@@ -12,6 +12,11 @@ baseUrl = "https://dev.renuva.com.py/login"
 driver = init_page(baseUrl)
 wait = WebDriverWait(driver, 8)
 
+
+## Defining variables to keep information
+
+errors_deleting = []
+
 ## Login
 email = driver.find_element(By.ID, 'email')
 password = driver.find_element(By.ID, 'password')
@@ -27,18 +32,33 @@ def checkUrlChanges(desired_url, url_jump):
     driver.get(url_jump)
     # driver.find_element(By.ID, 'dropClientes').click()
 
-def deleteUsers():
+def deleteUsers(index_deploy = 0, index_delete = 0):
     buttons_deploy = driver.find_elements(By.XPATH, '//*[@class="btn btn-primary listadeudas"]')
-    for i in range(1): 
-        buttons_deploy[i].click()
-        container_delete = driver.find_element(By.XPATH, '//*[@class="listadeudas no-hover"]')
-        buttons_trash = container_delete.find_elements(By.CSS_SELECTOR, "[title~='Eliminar']")
-        # '//*[@class="fa fa-trash"]//parent::button'
-        if len(buttons_trash) > 0:
-            buttons_trash[0].click()
-        # delete = 
-        driver.execute_script("arguments[0].click();",wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="btn btn-primary mt-4"]'))))
-        alert = wait.until(EC.alert_is_present())
-        alert.accept()
+        
+    next_index_delete = index_delete
+    next_index_deploy = index_deploy
+
+    buttons_deploy[index_deploy].click()
+    container_delete = driver.find_element(By.XPATH, '//*[@class="listadeudas no-hover"]')
+    buttons_trash = container_delete.find_elements(By.CSS_SELECTOR, "[title~='Eliminar']")
+    # '//*[@class="fa fa-trash"]//parent::button'
+    buttons_trash[index_delete].click()
+    # delete = 
+    driver.execute_script("arguments[0].click();",wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@class="btn btn-primary mt-4"]'))))
+    alert = wait.until(EC.alert_is_present())
+    alert.accept()
+    try:
+        element = wait.until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/header/div/div[2]/span[1]")))
+        errors_deleting.append(element.text)
+        print("errors ===> ", element.text)
+        driver.get('https://dev.renuva.com.py/clientes/duplicados')
+        if len(buttons_trash) - 1 > next_index_delete:
+            deleteUsers(next_index_deploy, next_index_delete + 1)
+        else:
+            deleteUsers(next_index_deploy + 1)
+    except Exception: 
+        print('Hola')
+
 checkUrlChanges('https://dev.renuva.com.py/clientes/agenda', 'https://dev.renuva.com.py/clientes/duplicados')
 deleteUsers()
+print(errors_deleting)
